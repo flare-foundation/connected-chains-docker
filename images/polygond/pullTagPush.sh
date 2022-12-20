d2f25584d0426d40a27e33ae5ac33d3ae544eff0
samclusker <9279784+samclusker@users.noreply.github.com>
2022-12-20 16:38:03 +0000

    Added config files and scripts to help get started with Ethereum and Polygon set ups.

---
+++ images/polygond/pullTagPush.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Heimdall
+docker pull registry.hub.docker.com/0xpolygon/bor:0.3.0 
+docker tag registry.hub.docker.com/0xpolygon/bor:0.3.0 registry.hub.docker.com/flarefoundation/bor:0.3.0
+docker push registry.hub.docker.com/flarefoundation/bor:0.3.0
+
+# Bor
+docker pull registry.hub.docker.com/0xpolygon/heimdall:0.3.0
+docker tag registry.hub.docker.com/0xpolygon/heimdall:0.3.0 registry.hub.docker.com/flarefoundation/heimdall:0.3.0
+docker push registry.hub.docker.com/flarefoundation/heimdall:0.3.0
