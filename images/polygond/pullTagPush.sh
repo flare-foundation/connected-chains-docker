4be1199f7c73bab5077d7b23ae9451e783d76bb1
samclusker <9279784+samclusker@users.noreply.github.com>
2022-12-21 10:20:29 +0000

    Copied dockerfiles or builder files.

--- images/polygond/pullTagPush.sh
+++ images/polygond/pullTagPush.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Heimdall
+# Bor
 docker pull registry.hub.docker.com/0xpolygon/bor:0.3.0 
 docker tag registry.hub.docker.com/0xpolygon/bor:0.3.0 registry.hub.docker.com/flarefoundation/bor:0.3.0
 docker push registry.hub.docker.com/flarefoundation/bor:0.3.0
 
-# Bor
+# Heimdall
 docker pull registry.hub.docker.com/0xpolygon/heimdall:0.3.0
 docker tag registry.hub.docker.com/0xpolygon/heimdall:0.3.0 registry.hub.docker.com/flarefoundation/heimdall:0.3.0
 docker push registry.hub.docker.com/flarefoundation/heimdall:0.3.0
