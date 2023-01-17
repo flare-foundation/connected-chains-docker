eb67c3ebe4d7ecdd6cc0e7fb8e6ae397d50b7efa
samclusker <9279784+samclusker@users.noreply.github.com>
2023-01-17 11:22:35 +0000

    Updated bor version

--- images/polygond/pullTagPush.sh
+++ images/polygond/pullTagPush.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Bor
-docker pull registry.hub.docker.com/0xpolygon/bor:0.3.0 
-docker tag registry.hub.docker.com/0xpolygon/bor:0.3.0 registry.hub.docker.com/flarefoundation/bor:0.3.0
-docker push registry.hub.docker.com/flarefoundation/bor:0.3.0
+docker pull registry.hub.docker.com/0xpolygon/bor:0.3.3 
+docker tag registry.hub.docker.com/0xpolygon/bor:0.3.3 registry.hub.docker.com/flarefoundation/bor:0.3.3
+docker push registry.hub.docker.com/flarefoundation/bor:0.3.3
 
 # Heimdall
 docker pull registry.hub.docker.com/0xpolygon/heimdall:0.3.0
