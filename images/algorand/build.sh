520b1de746b0edbad6f50b2fa56afd440e04e240
klemenfn <klemen@flare.network>
2023-06-28 13:25:09 +0200

    bump btc, algo and xrpl images, optimize btc size

--- images/algorand/build.sh
+++ images/algorand/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --progress=plain -t flarefoundation/algorand:3.14.2 .
+docker buildx build --progress=plain -t flarefoundation/algorand:3.16.3 .
--- images/rippled/build.sh
+++ images/rippled/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --progress=plain -t flarefoundation/rippled:1.10.0 .
+docker buildx build --progress=plain -t flarefoundation/rippled:1.11.0 .
