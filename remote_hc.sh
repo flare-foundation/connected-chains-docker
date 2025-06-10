005dc1bd8c1ff0a069e43a4f522ae8a548a1996f
Benjamin <benjamin102014@gmail.com>
2025-06-10 15:03:17 +0200

    update Algorand API to v2

--- remote_hc.sh
+++ remote_hc.sh
@@ -16,4 +16,4 @@ echo "====================== RIPPLED ======================"
 curl -X POST -m 10 -H "Content-type: application/json" -d '{"method": "server_info", "params":[{"api_version": 1}]}' http://$HOST:51234 |jq
 
 echo "====================== ALGORAND ======================"
-curl -X GET -m 10 http://$HOST:6332/v1/status -H "X-Algo-API-Token: $PASS" |jq
+curl -X GET -m 10 http://$HOST:6332/v2/status -H "X-Algo-API-Token: $PASS" |jq
