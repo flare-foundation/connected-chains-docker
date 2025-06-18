20f21c62c2a0052700138ab9e8c59303e55975da
Benjamin <benjamin102014@gmail.com>
2025-06-18 12:10:52 +0200

    Initial commit of entrypoint replacement in Go

---
+++ images/algorand/scripts/entrypoint.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+
+)
+
+const (
+	algorandDir = "/opt/algorand"
+	defaultsDir = "/opt/algorand/algorand-defaults"
+	defaultConfigDir = "/opt/algorand/default-config"
+)
+
+func main() {
+	
+}
