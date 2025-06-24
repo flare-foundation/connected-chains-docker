08e5a33f12a3b8ec96281f9319e64dd841b3e64d
Benjamin <benjamin102014@gmail.com>
2025-06-24 12:11:08 +0200

    Remove plain progress output

--- images/dogecoind/build_from_local_archive.sh
+++ images/dogecoind/build_from_local_archive.sh
@@ -1,1 +1,1 @@
-docker build --build-arg SOURCE_ZIP=dogecoin-1.14.8-flare.zip --build-arg SOURCE_FOLDER=dogecoin-1.14.8-flare --progress=plain -f Dockerfile-wls -t flarefoundation/dogecoin:1.14.8-wls .
+docker build --build-arg SOURCE_ZIP=dogecoin-1.14.8-flare.zip --build-arg SOURCE_FOLDER=dogecoin-1.14.8-flare -f Dockerfile-wls -t flarefoundation/dogecoin:1.14.8-wls .
